080cea4c291cf52fb7863265d0897818f6d63a7d
Kultigin Karagenc <karagenc.kultigin@gmail.com>
2020-11-05 16:51:00 +0100

    kultik update icons + show Movies big card

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,14 @@ var highlighted;
 var Featured;
 var featuredGenre;
 var nowTheatre;
+var shopMovies;
+var bigCard;
 
 
 // Templates
 
 var highlightedMovieCardHTMLcode =
-`<div class="col-lg-2">
+    `<div class="col-lg-2">
     <div class="card border-0" id="highlightCard">
     <div class="card-body">
         <figure class="figure justify-content-around row">
@@ -23,7 +25,7 @@ var highlightedMovieCardHTMLcode =
     </div>
 </div>`;
 var featuredMovieCardHTMLcode =
-`<div class="col-lg-2">
+    `<div class="col-lg-2">
     <div class="card border-0" id="highlightCard">
     <div class="card-body">
         <figure class="figure justify-content-around row">
@@ -36,8 +38,24 @@ var featuredMovieCardHTMLcode =
     </div>
     </div>
 </div>`;
+
+var shopMoviesCardHTMLcode =
+    `<div class="col-lg-3">
+    <div class="card border-0" id="highlightCard">
+    <div class="card-body">
+        <figure class="figure justify-content-around row">
+        <img src= _imgSrc class="figure-img img-fluid rounded col-12" alt="">
+        <figcaption class="figure-caption text-xs-right col-12 row">
+            <p class="card-title col-12"> _title </p>
+            <p class="card-text col-6 m-0 p-0" id="highlightYear"> _year </p>
+        </figcaption>
+        </figure> 
+    </div>
+    </div>
+</div>`;
+
 var carouselInnerHTMLcode =
-`<div class="carousel-item _tmp ">
+    `<div class="carousel-item _tmp ">
 <img class="d-block img-fluid" src= _imgSrc alt="First slide">
 <div class="carousel-caption d-none d-md-block">
   <h1 class="display-3">LATEST ONLINE MOVIES</h1>
@@ -48,30 +66,66 @@ var carouselInnerHTMLcode =
   </p>
 </div>`;
 
+var bigCardHTMLcode =
+    `<figure class="figure justify-content-around row">
+<img src=_imgSrc class=""d-block img-fluid" col-12" alt="">
+<figcaption class="figure-caption text-xs-right col-12 row">
+  <div class="container">
+    <div class="row">
+      <h3 class="card-title col-12">_title</h3>
+      <p class="card-text col-3 m-0 p-0" id="storyline">Story line</p>
+      <p class="card-text col-1 m-0 p-0" class="dot">:</p>
+      <p class="card-text col-8 m-0 p-0" id="summaryText"> _summary </p>
+    </div>
+    <div class="row">
+      <p class="card-text col-3 m-0 p-0" id="releaseOn">Release On</p>
+      <p class="card-text col-1 m-0 p-0" class="dot">:</p>
+      <p class="card-text col-8 m-0 p-0" id="releaseDate">_year</p>
+    </div>
+    <div class="row">
+      <p class="card-text col-3 m-0 p-0" id="Genres">Genres</p>
+      <p class="card-text col-1 m-0 p-0" class="dot">:</p>
+      <p class="card-text col-6 m-0 p-0" id="highlightGenre"> _genre </p>
+    </div>
+    <div class="row">
+      <p class="card-text col-3 m-0 p-0" id="price">Price</p>
+      <p class="card-text col-1 m-0 p-0" class="dot">:</p>
+      <p class="card-text col-8 m-0 p-0" id="priceTag">Price tag</p>
+    </div>
+  </div>
+</figcaption>
+</figure>`
+
+
 
 // Classes/Objects
 
 var TMDB = {
-    apiKey : "?api_key=766c1ba4a606493ccaf76431323f4829",
+    apiKey: "?api_key=766c1ba4a606493ccaf76431323f4829",
     apiOption: "&language=fr",
-    apiBaseURL : "http://api.themoviedb.org/3/",
-    apiImageBaseURL : "http://image.tmdb.org/t/p/",
-    youtubeBaseURL : "https://www.youtube.com/watch?v="
+    apiBaseURL: "http://api.themoviedb.org/3/",
+    apiImageBaseURL: "http://image.tmdb.org/t/p/",
+    youtubeBaseURL: "https://www.youtube.com/watch?v="
 };
 
 // Fetchs
-const fetchGenres = fetch('https://api.themoviedb.org/3/genre/movie/list'+TMDB.apiKey+TMDB.apiOption);  // Get all Genres with their id
-const fetchTopRated = fetch('https://api.themoviedb.org/3/movie/top_rated'+TMDB.apiKey+TMDB.apiOption); // Highlighted
-const fetchPopular = fetch('https://api.themoviedb.org/3/movie/popular'+TMDB.apiKey+TMDB.apiOption); // Featured
-const fetchNowPlaying = fetch('https://api.themoviedb.org/3/movie/popular'+TMDB.apiKey+TMDB.apiOption);
+const fetchGenres = fetch('https://api.themoviedb.org/3/genre/movie/list' + TMDB.apiKey + TMDB.apiOption);  // Get all Genres with their id
+const fetchTopRated = fetch('https://api.themoviedb.org/3/movie/top_rated' + TMDB.apiKey + TMDB.apiOption); // Highlighted
+const fetchPopular = fetch('https://api.themoviedb.org/3/movie/popular' + TMDB.apiKey + TMDB.apiOption); // Featured
+const fetchNowPlaying = fetch('https://api.themoviedb.org/3/movie/popular' + TMDB.apiKey + TMDB.apiOption);
+const fetchShowMovies = fetch('https://api.themoviedb.org/3/movie/now_playing' + TMDB.apiKey + TMDB.apiOption); // Shop Movies
+const fetchBigCard = fetch('https://api.themoviedb.org/3/movie/now_playing' + TMDB.apiKey + TMDB.apiOption); // Big Card
 
 
-Promise.all([fetchGenres, fetchTopRated, fetchPopular, fetchNowPlaying]).then(values=>{
-    return Promise.all(values.map(fetch=>fetch.json()))}).then(([genres, topRated, popular, nowPlaying])=>{
+Promise.all([fetchGenres, fetchTopRated, fetchPopular, fetchNowPlaying, fetchShowMovies, fetchBigCard]).then(values => {
+    return Promise.all(values.map(fetch => fetch.json()))
+}).then(([genres, topRated, popular, nowPlaying, showThem, card]) => {
     genreList = genres.genres;
     highlighted = topRated.results.slice(0, 5);
     Featured = popular.results.slice(0, 12);
     nowTheatre = nowPlaying.results;
+    shopMovies = showThem.results.slice(0, 8);
+    bigCard = card.results.slice(0, 1);
     //console.log(highlighted);
 
 
@@ -79,14 +133,16 @@ Promise.all([fetchGenres, fetchTopRated, fetchPopular, fetchNowPlaying]).then(va
     var html_highlightedMovies = document.getElementById('highlightedMoviesRow');
     var html_featuredMovies = document.getElementById('featuredMoviesRow');
     var html_NowTheatre = document.getElementById('carouselInner');
+    var html_shopMovies = document.getElementById('underShopMovies');
+    var html_bigCard = document.getElementById("theBigCard");
 
     // Build Highlighted Cards
-    highlighted.map((x) =>{
+    highlighted.map((x) => {
         let tmp = highlightedMovieCardHTMLcode;
         tmp = tmp.replace(/_title/, x.title);
-        tmp = tmp.replace(/_year/, x.release_date.substring(0,x.release_date.indexOf('-')));
+        tmp = tmp.replace(/_year/, x.release_date.substring(0, x.release_date.indexOf('-')));
         tmp = tmp.replace(/_genre/, getGenreName(x.genre_ids[0]));
-        tmp = tmp.replace(/_imgSrc/, TMDB.apiImageBaseURL+'w300'+x.poster_path+TMDB.apiKey+TMDB.apiOption);
+        tmp = tmp.replace(/_imgSrc/, TMDB.apiImageBaseURL + 'w300' + x.poster_path + TMDB.apiKey + TMDB.apiOption);
         html_highlightedMovies.insertAdjacentHTML('beforeend', tmp);
     });
 
@@ -94,42 +150,61 @@ Promise.all([fetchGenres, fetchTopRated, fetchPopular, fetchNowPlaying]).then(va
     var rawFeaturedGenre = [];
 
     // Build Featured Cards
-    Featured.map((x) =>{
+    Featured.map((x) => {
         rawFeaturedGenre.push(getGenreName(x.genre_ids[0]));
         let tmp = featuredMovieCardHTMLcode;
         tmp = tmp.replace(/_title/, x.title);
-        tmp = tmp.replace(/_year/, x.release_date.substring(0,x.release_date.indexOf('-')));
-        tmp = tmp.replace(/_imgSrc/, TMDB.apiImageBaseURL+'w300'+x.poster_path+TMDB.apiKey+TMDB.apiOption);
+        tmp = tmp.replace(/_year/, x.release_date.substring(0, x.release_date.indexOf('-')));
+        tmp = tmp.replace(/_imgSrc/, TMDB.apiImageBaseURL + 'w300' + x.poster_path + TMDB.apiKey + TMDB.apiOption);
         html_featuredMovies.insertAdjacentHTML('beforeend', tmp);
+    });
+
+    // Build Shop Movies Cards
+    shopMovies.map((x) => {
+        let tmp = shopMoviesCardHTMLcode;
+        tmp = tmp.replace(/_title/, x.title);
+        tmp = tmp.replace(/_year/, x.release_date.substring(0, x.release_date.indexOf('-')));
+        tmp = tmp.replace(/_imgSrc/, TMDB.apiImageBaseURL + 'w300' + x.poster_path + TMDB.apiKey + TMDB.apiOption);
+        html_shopMovies.insertAdjacentHTML('beforeend', tmp);
+    });
+
+    // Build Big Card
+    bigCard.map((x) => {
+        let tmp = bigCardHTMLcode;
+        tmp = tmp.replace(/_title/, x.title);
+        tmp = tmp.replace(/_summary/, x.overview);
+        tmp = tmp.replace(/_year/, x.release_date.substring(0, x.release_date.indexOf('-')));
+        tmp = tmp.replace(/_imgSrc/, TMDB.apiImageBaseURL + 'w500' + x.poster_path + TMDB.apiKey + TMDB.apiOption);
+        html_bigCard.insertAdjacentHTML('beforeend', tmp);
     });
 
     // Transform rawFeaturedGenre into an Array of unique genre
     featuredGenre = Array.from([...new Set(rawFeaturedGenre)]);
 
-    nowTheatre.map(async (x,index) =>{
+    nowTheatre.map(async (x, index) => {
         let tmp = carouselInnerHTMLcode;
-        if(index == 0) tmp = tmp.replace(/_tmp/, 'active');;
+        if (index == 0) tmp = tmp.replace(/_tmp/, 'active');;
         tmp = tmp.replace(/_title/, x.title);
-        tmp = tmp.replace(/_year/, x.release_date.substring(0,x.release_date.indexOf('-')));
-        tmp = tmp.replace(/_imgSrc/, TMDB.apiImageBaseURL+'original'+x.backdrop_path+TMDB.apiKey+TMDB.apiOption);
+        tmp = tmp.replace(/_year/, x.release_date.substring(0, x.release_date.indexOf('-')));
+        tmp = tmp.replace(/_imgSrc/, TMDB.apiImageBaseURL + 'original' + x.backdrop_path + TMDB.apiKey + TMDB.apiOption);
         let a = await getTrailer(x.id);
-        tmp = tmp.replace(/_href/, TMDB.youtubeBaseURL+a[0] );
+        tmp = tmp.replace(/_href/, TMDB.youtubeBaseURL + a[0]);
         html_NowTheatre.insertAdjacentHTML('beforeend', tmp);
     });
 
-    
-    function getGenreName(movieGenreId){
+
+    function getGenreName(movieGenreId) {
         return genreList.find(element => element.id == movieGenreId).name;
     }
 
     async function getTrailer(id) {
-        let res = await fetch('https://api.themoviedb.org/3/movie/'+id+'/videos?api_key=766c1ba4a606493ccaf76431323f4829&language=fr');  
+        let res = await fetch('https://api.themoviedb.org/3/movie/' + id + '/videos?api_key=766c1ba4a606493ccaf76431323f4829&language=fr');
         if (res.status == 200) {
-          let json = await res.json();
-          return json.results.map(x => x.key);
+            let json = await res.json();
+            return json.results.map(x => x.key);
         }
         throw new Error(res.status);
-      }
+    }
 
 
 
